f059de2f0c9285c2d893ff2e99d845c4d7713095
keeor <masqmoreforjob@gmail.com>
2023-05-31 18:01:24 +0300

    Add files

--- main.py
+++ main.py
@@ -1,29 +1,134 @@
+'''
+Сервіс – «Кошик споживача».
+Задача сервісу виконати підрахунок вартості кошика споживача за обраними
+товарами на вказаних торгівельних майданчиках.
+Клієнт сервісу:
+a. формує кошик, вводить (обирає) перелік товарів;
+b. обирає перелік супермаркетів, які будуть приймати участь у
+формуванні кошика.
+Сервіс:
+c. видає інформацію про загальну вартість товарів по кожному
+обраному торгівельному майданчику (з приміткою про відсутність
+незнайдених товарів);
+d. забезпечує можливість перегляду детальної інформації по обраним
+товарам для кожного торгівельного майданчика;
+надає інформацію про адресу та режим роботи торгівельного майданчика
+'''
+
 import tkinter as tk
-from db import geting_info
+from tkinter import *
+from db import geting_info as gi
+from tkinter.messagebox import showinfo
 
 
-stores_list = geting_info.stores_list()
-products_list = geting_info.products_list()
-shopping_cart =[]
-
+stores_list = gi.stores_list()
+products_list = gi.products_list()
+store_name_list = gi.store_list
+cart = gi.ShoppingCart()
+prod_lst = gi.list_inf()
+#Создание интерфейса
 root = tk.Tk()
 root.title("Сервіс - Кошик споживача")
 
+#Создание чек-листа продуктов
 frame_products = tk.Frame(root)
 frame_products.pack(side=tk.LEFT, padx=10)
 scrollbar_product_list = tk.Scrollbar(frame_products)
 scrollbar_product_list.pack(side=tk.RIGHT, fill=tk.Y)
-
 listbox_product_list = tk.Listbox(frame_products, yscrollcommand=scrollbar_product_list.set)
-print(products_list)
-for product in products_list:
-    listbox_product_list.insert(tk.END, f"{product[0]} {product[1]}")
-    listbox_product_list.pack(side=tk.LEFT, fill=tk.BOTH)
 
-scrollbar_product_list.config(command=listbox_product_list.yview)
+for product in products_list:
+    listbox_product_list.insert(tk.END, f"{product[0]}")
+    listbox_product_list.pack(side=tk.LEFT, fill=tk.BOTH)
+    scrollbar_product_list.config(command=listbox_product_list.yview)
+listbox_product_list.insert(tk.END, ' ')
 
 frame_buttons = tk.Frame(root)
 frame_buttons.pack(side=tk.LEFT, padx=10)
 
+
+frame_list = tk.Frame(root)
+frame_list.pack(side=tk.LEFT, padx=10)
+
+def default_list():
+    listbox_product_list.delete(0, tk.END)
+    for product in products_list:
+        listbox_product_list.insert(tk.END, f"{product[0]} ")
+        listbox_product_list.pack(side=tk.LEFT, fill=tk.BOTH)
+        scrollbar_product_list.config(command=listbox_product_list.yview)
+    listbox_product_list.insert(tk.END, ' ')
+
+button_list = tk.Button(frame_list, text="All", command=lambda: default_list())
+button_list.pack(pady=5)
+
+
+def add_to_cart():
+    # Функція додавання продукту до кошика
+    selected_ind = listbox_product_list.curselection()
+    i = int(selected_ind[0])
+    product = products_list[i][0]
+    cart.add_product(product)
+    print(cart.products)
+
+
+button_add_to_cart = tk.Button(frame_buttons, text="Додати в кошик", command=lambda: add_to_cart())
+button_add_to_cart.pack(pady=5)
+
+frame_filter = tk.Frame(root)
+frame_filter.pack(side=tk.LEFT, padx=10)
+
+label_filter = tk.Label(frame_filter, text="Фільтр")
+label_filter.pack()
+
+
+def filtered_list(sto):
+    listbox_product_list.delete(0, tk.END)
+    for product in sto:
+        listbox_product_list.insert(tk.END, f"{product[0]} ")
+        listbox_product_list.pack(side=tk.LEFT, fill=tk.BOTH)
+        scrollbar_product_list.config(command=listbox_product_list.yview)
+    listbox_product_list.insert(tk.END, ' ')
+
+new_list = []
+def filter_products():
+    print('pass')
+    # Функція фільтрації продуктів за магазином
+    selected_items = [item.get() for item in checkbox_vars]
+    filtered_items = [stores_list[0] for stores_list, selected in zip(stores_list, selected_items) if selected]
+    print(filtered_items)
+    for i in range(len(prod_lst)):
+        if prod_lst[i].name in filtered_items:
+            for j in range(len(prod_lst[i].product_list)):
+                if prod_lst[i].product_list[j][0] not in new_list:
+                    new_list.append(prod_lst[i].product_list[j][0])
+                    print(new_list)
+        else:
+
+
+
+
+checkbox_vars = []
+for item in gi.stores_list():
+    var = tk.BooleanVar()
+    checkbox_vars.append(var)
+    checkbox = tk.Checkbutton(frame_filter, text=item[0], variable=var)
+    checkbox.pack(side=tk.LEFT)
+
+
+button_compare = tk.Button(frame_buttons, text="Порівняти", command=lambda: filter_products())
+button_compare.pack(pady=5)
+var = StringVar()
+
+def calculate_total_cost():
+    # Функція розрахунку загальної вартості кошика
+    total_cost = cart.calculate_total_cost()
+    label_total_cost.config(text=f"Загальна вартість: {total_cost} грн.")
+
+
+button_calculate_total_cost = tk.Button(root, text="Підрахувати загальну вартість", command=calculate_total_cost)
+button_calculate_total_cost.pack(pady=10)
+
+label_total_cost = tk.Label(root, text="Загальна вартість: ")
+label_total_cost.pack()
 
 root.mainloop()
--- main_data/geting_info.py
+++ main_data/geting_info.py
@@ -1,26 +1,91 @@
 import pandas as pd
 
-stores = []
-products_l = []
 
+class Store:
+    def __init__(self, name, adress, work_time):
+        self.name = name
+        self.adress = adress
+        self.work_time = work_time
+        self.product_list = []
 
-def products_list():
-    excel_data = pd.read_excel('main_data/products_list.xlsx')
-    data = pd.DataFrame(excel_data)
-    for i in range(len(data['Name'])):
-        print(len(data))
-        print(i)
-        product = [data['Name'][i], data['Cost'][i]]
-        print(data['Name'][i], data['Cost'][i])
-        products_l.append(product)
-    return products_l
+    def get_name(self, cls):
+        Store.name_list.append(self.name)
+
+    def ret_names(self):
+        return self.name_list
+
+    def prod_list_data(self, product):
+        self.product_list.append(product)
 
 
 def stores_list():
+    stores = []
     excel_data = pd.read_excel('main_data/stores_list.xlsx')
     data = pd.DataFrame(excel_data)
     for i in range(len(data['Name'])):
         store = [data['Name'][i], data['Adress'][i], data['Work Time'][i]]
         stores.append(store)
     return stores
+
+
+data = stores_list()
+store_list = []
+names = []
+address = []
+work_time = []
+products_l = []
+
+# Создание класов в елементах списка и заполнение их инфой
+
+for name in data:
+    store_list.append(name)
+for i in range(len(store_list)):
+    names.append(store_list[i][0])
+    address.append(store_list[i][1])
+    work_time.append(store_list[i][2])
+    store_list[i] = Store(names[i], address[i], work_time[i])
+
+# Заполнение списка продуктов каждого магазина
+for i in range(len(store_list)):
+    Name = store_list[i].name
+    for stor in store_list:
+        if stor.name == store_list[i].name:
+            excel_data = pd.read_excel(f'main_data/{Name} products_list.xlsx')
+            data = pd.DataFrame(excel_data)
+            for j in range(len(data)):
+                product = [data['Name'][j], data['Cost'][j]]
+                store_list[i].prod_list_data(product)
+def list_inf():
+    return store_list
+################################################
+def products_list():
+    products_l = []
+    excel_data = pd.read_excel('main_data/products_list.xlsx')
+    data = pd.DataFrame(excel_data)
+    for i in range(len(data['Name'])):
+        product = [data['Name'][i], data['Cost'][i]]
+        products_l.append(product)
+    return products_l
+
+
+class ShoppingCart:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def calculate_total_cost(self):
+        total_cost = sum(self.products)
+        print(self.products)
+        return total_cost
+
+
+
+def compare_products(product):
+    # Функція порівняння продукту з іншим магазином
+    pass
+
+checkbox_vars = []
+
 
